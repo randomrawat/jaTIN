62f0e0783aa8b43975547dd5f36fbce9330f2abf
Shubham Rawat <shubhamrawat8249@gmail.com>
2024-09-13 22:54:40 +0530

    Add files via upload

---
+++ jatin.js
@@ -0,0 +1,14 @@
+function yes(){
+    window.open('loveydovey.html','_self');
+
+}
+
+function no(){
+    window.open('jatin2.html','_self');
+
+}
+
+function no2(){
+    window.open('index.html','_self');
+
+}
